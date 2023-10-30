992afcddaea9be8365076f472ab328c14e07a0be
Justinas Simas <simjustinas@gmail.com>
2023-10-30 22:00:32 +0200

    updated initial users passwords

---
+++ karma-api/src/main/resources/db/migration/V3__initial_users_password_update.sql
@@ -0,0 +1,2 @@
+UPDATE users SET password = '$2a$10$6iJkmGJCEE3zZor.Df53uON14zcRpI9Lb5nQ4T7MvqQG0upaH3.GS' WHERE email = 'volunteer@karma.com';
+UPDATE users SET password = '$2a$10$JzX8CKlGe94YyO3feulk4OBh19dIDrpTFR0Q7furI6nxEKH7N08J2' WHERE email = 'admin@karma.com';
