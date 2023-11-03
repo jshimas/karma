5aced8e8e2db8589d344a9ff28cdf4045ea5a37a
Justinas Simas <simjustinas@gmail.com>
2023-11-03 16:11:48 +0200

    checking out db connections

--- karma-api/src/main/java/com/jshimas/karmaapi/KarmaApiApplication.java
+++ karma-api/src/main/java/com/jshimas/karmaapi/KarmaApiApplication.java
@@ -1,15 +1,25 @@
 package com.jshimas.karmaapi;
 
 import com.bedatadriven.jackson.datatype.jts.JtsModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KarmaApiApplication {
+    @Value("${spring.datasource.url}")
+    private static String url;
+    @Value("${spring.datasource.password}")
+    private static String pass;
+    @Value("${spring.datasource.username}")
+    private static String username;
 
     public static void main(String[] args) {
         SpringApplication.run(KarmaApiApplication.class, args);
+        System.out.println(url);
+        System.out.println(username);
+        System.out.println(pass);
     }
 
     @Bean
